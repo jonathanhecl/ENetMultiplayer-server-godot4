2707c539663e484a7fd2563d3c19eadebf8beff3
Jonathan Hecl <jonathanhecl@gmail.com>
2023-12-01 17:44:31 -0300

    better server logic

--- scenes/main.gd
+++ scenes/main.gd
@@ -1,19 +1,38 @@
 extends Node2D
 
-var server := ENetMultiplayerPeer.new()
+var peer := ENetMultiplayerPeer.new()
 
 func _ready():
-	var error = server.create_server(7070)
-	if error != OK: print("ERROR CODE: ", error)
-	multiplayer.multiplayer_peer = server
+	multiplayer.peer_connected.connect(_on_client_connect)
+	multiplayer.peer_disconnected.connect(_on_client_disconnect)
+
+	var error = peer.create_server(7070)
+	if error != OK: 
+		print("Error ", str(error))
+	multiplayer.multiplayer_peer = peer
+
+func _on_client_connect(peer_id):
+	$ItemList.add_item(str(peer_id) + " connected")
+
+func _on_client_disconnect(peer_id):
+	$ItemList.add_item(str(peer_id) + " disconnected")
 
 func _process(delta):
-	var status = server.get_connection_status()
-	if status == server.CONNECTION_CONNECTED:
+	var status = peer.get_connection_status()
+	if status == peer.CONNECTION_CONNECTED:
 		$Status.text = "Listen 7070"
-	elif status == server.CONNECTION_DISCONNECTED:
+	elif status == peer.CONNECTION_DISCONNECTED:
 		$Status.text = "Offline"
 	
-	var peers = server.get_host().get_peers()
+	var peers = peer.get_host().get_peers()
 	$Clients.text = "Clients: " + str(len(peers)) + " => " + str(peers)
 
+@rpc("any_peer")
+func send_message_to_server(message: String):
+	print("send_message_to_server: ", message)
+	if multiplayer.is_server():
+		send_message_to_client.rpc(message)
+
+@rpc("authority")
+func send_message_to_client(message: String):
+	print("send_message_to_client", message)
