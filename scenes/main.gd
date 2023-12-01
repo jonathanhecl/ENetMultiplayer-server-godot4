29d1c96ea5c67ba751c1460e5219ffea412378f5
Jonathan Hecl <jonathanhecl@gmail.com>
2023-12-01 17:00:13 -0300

    server

--- scenes/main.gd
+++ scenes/main.gd
@@ -1,39 +1,17 @@
 extends Node2D
 
-var network := ENetMultiplayerPeer.new()
-var gateway := SceneMultiplayer.new()
+var server := ENetMultiplayerPeer.new()
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	network.peer_connected.connect(_on_peer_connected)
-	network.peer_disconnected.connect(_on_peer_disconnected)
-	
-	var error = network.create_server(7070)
-	if error: print("ERROR CODE: ", error)
-	get_tree().set_multiplayer(gateway, self.get_path())
-	multiplayer.set_multiplayer_peer(network)
-	
-	print("Server listen 7070")
+	var error = server.create_server(7070)
+	if error != OK: print("ERROR CODE: ", error)
 
 func _process(delta):
-	if gateway.has_multiplayer_peer():
-		gateway.poll()
-
-func _on_peer_connected(peer_id):
-	print("_on_peer_connected, peer_id: {0}".format([peer_id]))
-	await get_tree().create_timer(1).timeout
-	print("Custom Peers: {0}".format([multiplayer.get_peers()]))
+	var status = server.get_connection_status()
+	if status == server.CONNECTION_CONNECTED:
+		$Status.text = "Listen 7070"
+	elif status == server.CONNECTION_DISCONNECTED:
+		$Status.text = "Offline"
 	
-func _on_peer_disconnected(peer_id):
-	print("_on_peer_disconnected, peer_id: {0}".format([peer_id]))
-
-@rpc("any_peer") 
-func rpc_server_custom():
-	var peer_id = multiplayer.get_remote_sender_id() # even custom uses default "multiplayer" calls
-	print("rpc_server_custom, peer_id: {0}".format([peer_id]))
-	rpc_server_custom_response(peer_id)
-
-@rpc 
-func rpc_server_custom_response(peer_id, test_var1 : String = "party like it's", test_var2 : int = 1999):
-	print("rpc_server_custom_response to peer_id : {0}".format([peer_id]))
-	rpc_server_custom_response.rpc_id(peer_id, test_var1, test_var2)
+	var peers = server.get_host().get_peers()
+	$Clients.text = "Clients: " + str(len(peers)) + " => " + str(peers)
