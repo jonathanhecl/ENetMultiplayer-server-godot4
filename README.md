e9c954c9eb66dc542fdad1619cc4175bc58072bd
Jonathan Hecl <jonathanhecl@gmail.com>
2023-12-18 14:30:42 -0300

    rpc messages

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # ENetMultiplayer-server-godot4
 ENetMultiplayer Server example in Godot 4
+
+![Server screenshot](server.png)
--- scenes/main.gd
+++ scenes/main.gd
@@ -9,10 +9,13 @@ func _ready():
 	var error = peer.create_server(7070)
 	if error != OK: 
 		print("Error ", str(error))
+
 	multiplayer.multiplayer_peer = peer
 
 func _on_client_connect(peer_id):
 	$ItemList.add_item(str(peer_id) + " connected")
+	send_message_to_client.rpc_id(peer_id, "Hola Cliente, soy don Server")
+	
 
 func _on_client_disconnect(peer_id):
 	$ItemList.add_item(str(peer_id) + " disconnected")
@@ -27,12 +30,12 @@ func _process(delta):
 	var peers = peer.get_host().get_peers()
 	$Clients.text = "Clients: " + str(len(peers)) + " => " + str(peers)
 
-#@rpc("any_peer")
-#func send_message_to_server(message: String):
-	#print("send_message_to_server: ", message)
-	#if multiplayer.is_server():
-		#send_message_to_client.rpc(message)
-#
-#@rpc("authority")
-#func send_message_to_client(message: String):
-	#print("send_message_to_client", message)
+@rpc("any_peer")
+func send_message_to_server(message: String):
+	print("send_message_to_server: ", message)
+	if multiplayer.is_server():
+		send_message_to_client.rpc(message)
+		
+@rpc("authority")
+func send_message_to_client(message: String):
+	print("send_message_to_client", message)
